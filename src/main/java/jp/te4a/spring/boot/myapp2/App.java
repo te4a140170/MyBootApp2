b86aebda59d1b78c143c89ad34ecacb1fa2c31a4
田口 裕矢 <140170@jc-21.jp>
2018-05-16 16:24:31 +0900

    太郎君

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/App.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Taro desu!" );
+    }
+}
