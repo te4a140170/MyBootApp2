57a660e9d4e4b6720b7a1e796b2c928644550e0c
田口 裕矢 <140170@jc-21.jp>
2018-05-17 09:22:36 +0900

    太郎君2

--- src/main/java/jp/te4a/spring/boot/myapp2/App.java
+++ src/main/java/jp/te4a/spring/boot/myapp2/App.java
@@ -1,13 +1,21 @@
 package jp.te4a.spring.boot.myapp2;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 /**
  * Hello world!
  *
  */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Taro desu!" );
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
     }
 }
